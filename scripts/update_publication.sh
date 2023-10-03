9b6cb0eefd803b0d0394fb04eafc2f0315893fd2
Alan Guedes <alanlivio@outlook.com>
2023-10-03 18:52:35 +0100

    fix update_publication.sh

--- scripts/update_publication.sh
+++ scripts/update_publication.sh
@@ -6,4 +6,4 @@ test -f /tmp/alan_publication.bib && rm -r /tmp/alan_publication.bib
 curl "https://dblp.org/pid/141/9540.bib?param=1" -o /tmp/alan_publication.bib
 # import to content/publication
 test -d content/publication && rm -r content/publication
-academic import --bibtex /tmp/alan_publication.bib --publication-dir content/publication --overwrite
+academic import --bibtex /tmp/alan_publication.bib --publication-dir content/publications --overwrite
