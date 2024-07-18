77167c132ee78778317f830f0fe68b11609807ac
Alan Guedes <alanlivio@outlook.com>
2024-07-18 15:17:30 +0100

    fix publication script and use python3.12

--- scripts/update_publication.sh
+++ scripts/update_publication.sh
@@ -3,11 +3,12 @@
 
 # install hugo academic cli
 if ! type academic; then
-    pip3 install -U academic
+    pip3 install -U academic==0.11.2
 fi
 # fetch bib from dbpl
-test -f /tmp/alan_publication.bib && rm -r /tmp/alan_publication.bib
-curl "https://dblp.org/pid/141/9540.bib?param=1" -o /tmp/alan_publication.bib
+test -f /tmp/alan_publications.bib && rm -r /tmp/alan_publications.bib
+curl "https://dblp.org/pid/141/9540.bib?param=1" -o /tmp/alan_publications.bib
 # import to content/publication
-test -d content/publication && rm -r content/publication
-academic import --bibtex /tmp/alan_publication.bib --publication-dir content/publications --overwrite
+test -d content/publications && rm -r content/publications
+academic import --bibtex /tmp/alan_publications.bib --publication-dir content/publications --overwrite
+
